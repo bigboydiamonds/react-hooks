0921c0591880d3c79ebc9f859bfb8b71b637348a
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-08-04 13:46:45 -0700

    complete exercise 02

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,20 +3,20 @@
 
 import * as React from 'react'
 
-function Greeting() {
+function Greeting({initialName = ''}) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const [name, setName] = React.useState('')
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
-    setName(name => event.target.value)
+    setName(event.target.value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -24,7 +24,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Jonah" />
 }
 
 export default App
